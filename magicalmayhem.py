150e9bbaca46e402d0a4bfbbcc7954555f9cebad
Retroghost4 <retrospectart4@gmail.com>
2025-11-18 13:32:29 -0600

    Update magicalmayhem.py

--- magicalmayhem.py
+++ magicalmayhem.py
@@ -1,1 +1,16 @@
+import pygame
+import pygame_menu
+import sys
+import random
 
+pygame.init()
+surface = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+menu = pygame_menu.Menu('Welcome...' , 500, 300, 
+                        theme=pygame_menu.themes.THEME_RED)
+
+
+while True: 
+  for event in pygame.event.get(): 
+    if event.type == pygame.QUIT: 
+      pygame.quit()
+      sys.exit()
