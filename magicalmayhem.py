0b7c5e382c735efe629ae14c99cfc35aebc2519d
Retroghost4 <retrospectart4@gmail.com>
2025-12-06 19:34:18 -0600

    Update magicalmayhem.py

--- magicalmayhem.py
+++ magicalmayhem.py
@@ -5,12 +5,20 @@ Width, Height = 2000, 900
 window = pygame.display.set_mode((Width, Height))
 pygame.display.set_caption("The Magical Mayhem")
 
+BG = pygame.transform.scale(pygame.image.load("IMG_0675"), (Width, Height))
+
+def draw(): 
+  window.blit(BG, (0, 0))
+  pygame.display.update()
+
 def main(): 
   run = True
   while run: 
     for event in pygame.event.get(): 
       run = False
       break
+
+    draw()
 
   pygame.quit()
 
