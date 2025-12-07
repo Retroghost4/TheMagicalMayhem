9bd05063bcf1f4936a15c8e9fc26f190578fb956
Retroghost4 <retrospectart4@gmail.com>
2025-12-06 20:37:17 -0600

    Add files via upload

---
+++ magicalmayhem.py
@@ -0,0 +1,27 @@
+import pygame
+import random
+
+Width, Height = 2000, 900
+window = pygame.display.set_mode((Width, Height))
+pygame.display.set_caption("The Magical Mayhem")
+
+BG = pygame.transform.scale(pygame.image.load("IMG_0675"), (Width, Height))
+
+def draw(): 
+  window.blit(BG, (0, 0))
+  pygame.display.update()
+
+def main(): 
+  run = True
+  while run: 
+    for event in pygame.event.get(): 
+      run = False
+      break
+
+    draw()
+
+  pygame.quit()
+
+if __name__ = "__main__": 
+  main()
+
